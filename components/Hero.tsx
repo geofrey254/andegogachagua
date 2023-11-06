395c8f2e9eeb589d8535d9724e7aca9be169bdde
--replace-all <devongeoffreymaina@gmail.com>
2023-11-06 20:33:28 +0300

    Changed font family

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <div
-      className={["relative isolate px-6 pt-14 lg:px-8", styles.hero].join(" ")}
+      className={["relative isolate px-6 pt-14 md:px-8", styles.hero].join(" ")}
     >
       <div
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"

58ca2d67b1d0b7138b14ee589fe121d3cb0323e4
geofrey <devongeoffreymaina@gmail.com>
2024-01-30 15:29:11 +0300

    changed the sitemap file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://andegogachagua.com',
+      url: 'https://www.andegogachagua.com',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://andegogachagua.com/about',
+      url: 'https://www.andegogachagua.com/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://andegogachagua.com/practice_areas',
+      url: 'https://www.andegogachagua.com/practice_areas',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
     },
     {
-        url: 'https://andegogachagua.com/contact',
+        url: 'https://www.andegogachagua.com/contact',
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.5,
